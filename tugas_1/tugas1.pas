5cf6a80506e2800b045ee3dafba84aecbce20010
boks01 <msal40755@gmail.com>
2021-08-19 15:50:43 +0700

    adding tasks

---
+++ tugas_1/tugas1.pas
@@ -0,0 +1,13 @@
+program latihan;
+uses crt;
+var nama:string;
+var sekolah:string;
+begin
+clrscr;
+write('Nama: ');
+readln(nama);
+write('Asal sekolah: ');
+readln(sekolah);
+writeln('Hai ', nama,' dari ', sekolah,'.');
+readln();
+end .
